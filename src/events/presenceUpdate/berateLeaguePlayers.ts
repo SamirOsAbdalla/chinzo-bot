bd213af746dde1c2303fb966665fc7b478af70b7
samir01 <samir.os.abdalla@gmail.com>
2023-08-28 13:54:27 -0700

    If a user plays league they will be personally dealt with by Chinzo

---
+++ src/events/presenceUpdate/berateLeaguePlayers.ts
@@ -0,0 +1,11 @@
+import { Client, Presence } from "discord.js"
+
+
+module.exports = async (client: Client, presence: Presence) => {
+    presence.activities.forEach(activity => {
+        if (activity.name == "League of Legends") {
+            const user = client.users.cache.get(presence.userId)
+            user?.send("Get off league right now")
+        }
+    })
+}
