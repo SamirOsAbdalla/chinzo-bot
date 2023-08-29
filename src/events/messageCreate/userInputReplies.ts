66107e205b96edde547c4cadcd86ad50e495c8a9
samir01 <samir.os.abdalla@gmail.com>
2023-08-29 13:55:46 -0700

    Made event more modular so that in the future I could add more replies

--- src/events/messageCreate/userInputReplies.ts
+++ src/events/messageCreate/userInputReplies.ts
@@ -1,13 +1,57 @@
 import { Client, Message } from "discord.js"
 
 
-module.exports = async (client: Client, message: Message) => {
-    const content = message.content
-    if (content.includes("chin")) {
-        await message.reply("I AM CHINZO")
+const createResponseMessage = (content: string, substring: string): string => {
+    let numOccurrences = 0;
+    let stepSize = 1;
+    let position = 0;
+    while (true) {
+        position = content.indexOf(substring, position);
+        if (position >= 0) {
+            ++numOccurrences;
+            position += stepSize;
+        } else {
+            break;
+        }
     }
 
-    if (content.includes("ping")) {
-        await message.reply("Pong!")
+    let submitString = ""
+    switch (substring) {
+        case ("chin"): {
+            for (let i = 0; i < numOccurrences; i++) {
+                if (i == 0) {
+                    submitString += "I AM CHINZO"
+                } else {
+                    submitString += " CHINZO"
+                }
+            }
+            break;
+        }
+        case ("ping"): {
+            for (let i = 0; i < numOccurrences; i++) {
+                if (i == 0) {
+                    submitString += "Pong"
+                } else {
+                    submitString += " Pong"
+                }
+            }
+            break;
+        }
+        default:
+            break;
+    }
+
+    return submitString
+}
+module.exports = async (client: Client, message: Message) => {
+
+    const content = message.content.toLowerCase()
+    if (!message.author.bot && content.includes("chin")) {
+
+        await message.reply(createResponseMessage(content, "chin"))
+    }
+
+    if (!message.author.bot && content.includes("ping")) {
+        await message.reply(createResponseMessage(content, "ping"))
     }
 }
