f020048dd2a39eb72cdfc4d3b554a3aec50dafb3
samir01 <samir.os.abdalla@gmail.com>
2023-08-30 12:06:08 -0700

    Created command that allows users to play a song from youtube

---
+++ src/commands/music/play.ts
@@ -0,0 +1,43 @@
+import { Player } from "discord-player"
+import {
+    Client, ChatInputCommandInteraction,
+    ApplicationCommandOptionType, EmbedBuilder
+} from "discord.js"
+
+module.exports = {
+    name: "play",
+    description: "Play a video from youtube",
+    // devOnly: ,
+    // testOnly: ,
+    options: [
+        {
+            name: "query",
+            description: "Type in a song to query",
+            required: true,
+            type: ApplicationCommandOptionType.String
+        }
+    ],
+    callback: async (client: Client, interaction: any, player: Player) => {
+        const channel = interaction.member.voice.channel;
+        if (!channel) return interaction.reply('You are not connected to a voice channel!'); // make sure we have a voice channel
+
+        const query = interaction.options.getString('query', true); // we need input/query to play
+
+        // let's defer the interaction as things can take time to process
+        await interaction.deferReply();
+
+        try {
+            const { track } = await player.play(channel, query, {
+                nodeOptions: {
+                    // nodeOptions are the options for guild node (aka your queue in simple word)
+                    metadata: interaction // we can access this metadata object using queue.metadata later on
+                }
+            });
+
+            return interaction.followUp(`**${track.title}** enqueued`);
+        } catch (e) {
+            // let's return error if something failed
+            return interaction.followUp(`Something went wrong: ${e}`);
+        }
+    }
+}
