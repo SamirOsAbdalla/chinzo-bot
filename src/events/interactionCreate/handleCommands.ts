3a4b140f7764bc0dff48110c9636153f94cefe6e
samir01 <samir.os.abdalla@gmail.com>
2023-08-30 12:07:12 -0700

    Creates player object on initial load

--- src/events/interactionCreate/handleCommands.ts
+++ src/events/interactionCreate/handleCommands.ts
@@ -1,8 +1,99 @@
-import { Client, Interaction } from "discord.js"
+import {
+    Client, Interaction, EmbedBuilder,
+    ButtonBuilder, ButtonStyle, ActionRowBuilder,
+    ComponentType, ButtonInteraction, CacheType,
+    InteractionResponse, userMention
+} from "discord.js"
 import { testServer, devs } from "../../../config.json"
 import getLocalCommands from "../../utils/getLocalCommands";
+import { Player, GuildQueue, Track } from "discord-player";
+import { YouTubeExtractor } from "@discord-player/extractor";
+
+let i = 0;
+let player: Player;
+const playerMusicLogic = async (client: Client, interaction: any, queue: GuildQueue<any>, track: Track<unknown>) => {
+
+    const skipButton = new ButtonBuilder()
+        .setCustomId('skip')
+        .setLabel(`Skip`)
+        .setStyle(ButtonStyle.Secondary)
+        .setEmoji({ name: "⏩" })
+
+    const pauseButton = new ButtonBuilder()
+        .setCustomId('pause')
+        .setLabel(`Pause`)
+        .setStyle(ButtonStyle.Secondary)
+        .setEmoji({ name: "⏯️" })
+
+    const row = new ActionRowBuilder<ButtonBuilder>()
+        .addComponents(skipButton, pauseButton);
+
+    const videoEmbed = new EmbedBuilder()
+        .setColor("#d59342")
+        .setTitle(`Playing: ${track.title}`)
+        .setThumbnail(track.thumbnail)
+        .setDescription(`---------00:00/${track.duration}`)
+        .addFields({ name: '\u200B', value: '\u200B' }, { name: 'Requester', value: `${interaction.user.username}` })
+
+
+    // we will later define queue.metadata object while creating the queue
+    const response: InteractionResponse<boolean> = await queue.metadata.channel.send({ embeds: [videoEmbed], components: [row] });
+    try {
+
+        const collector = response.createMessageComponentCollector({ componentType: ComponentType.Button, time: track.durationMS })
+
+        collector.on("collect", async (i: ButtonInteraction<CacheType>) => {
+            switch (i.customId) {
+                case ("skip"): {
+                    skipButton.setDisabled(true)
+                    pauseButton.setDisabled(true)
+                    i.update({ components: [row] })
+                    queue.node.skip();
+                    break;
+                }
+                case ("pause"): {
+                    if (queue.node.isPaused()) {
+                        await i.reply({ content: `<@${i.user.id}> | You have unpaused. Enjoy!`, ephemeral: true })
+                        queue.node.resume()
+
+                    } else {
+                        await i.reply({
+                            content: `<@${i.user.id}> |  You have paused.`, ephemeral: true,
+                        })
+                        queue.node.pause();
+                    }
+                    break;
+                }
+                default:
+                    break;
+            }
+        })
+
+        collector.on("end", async (collected: any) => {
+            await response.delete()
+        })
+    } catch (e) {
+        console.log(e)
+        await interaction.editReply({ content: 'Error with video' });
+    }
+};
+
+
+
 
 module.exports = async (client: Client, interaction: any) => {
+
+    if (i == 0) {
+        i++;
+        player = new Player(client)
+        await player.extractors.register(YouTubeExtractor, {})
+
+
+        player.events.on('playerStart', (queue, track) => {
+            playerMusicLogic(client, interaction, queue, track)
+        });
+    }
+
     if (!interaction.isChatInputCommand()) {
         return;
     }
@@ -51,7 +142,13 @@ module.exports = async (client: Client, interaction: any) => {
             }
         }
 
-        await commandObject.callback(client, interaction)
+        if (interaction.commandName == "play") {
+            await commandObject.callback(client, interaction, player)
+        } else {
+            await commandObject.callback(client, interaction)
+
+        }
+
     } catch (error) {
         console.log(`Error in handle commands ${error}`)
     }
