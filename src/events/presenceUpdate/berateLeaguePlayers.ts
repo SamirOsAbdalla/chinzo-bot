3a524a67e59c2e90c1278af4e5ba1428bdad60ba
samir01 <samir.os.abdalla@gmail.com>
2023-08-31 12:59:59 -0700

    Temporarily deleted since it is annoying

--- src/events/presenceUpdate/berateLeaguePlayers.ts
+++ src/events/presenceUpdate/berateLeaguePlayers.ts
@@ -2,10 +2,10 @@ import { Client, Presence } from "discord.js"
 
 
 module.exports = async (client: Client, presence: Presence) => {
-    presence?.activities.forEach(activity => {
-        if (activity.name == "League of Legends") {
-            const user = client.users.cache.get(presence.userId)
-            user?.send("Get off league right now")
-        }
-    })
+    // presence?.activities.forEach(activity => {
+    //     if (activity.name == "League of Legends") {
+    //         const user = client.users.cache.get(presence.userId)
+    //         user?.send("Get off league right now")
+    //     }
+    // })
 }
