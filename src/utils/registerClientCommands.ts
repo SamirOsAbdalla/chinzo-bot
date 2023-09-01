86dce7c5b50267f3c49ed45ac45c5c8977df7d90
samir01 <samir.os.abdalla@gmail.com>
2023-09-01 10:53:00 -0700

    Populates global command array

---
+++ src/utils/registerClientCommands.ts
@@ -0,0 +1,27 @@
+import { Client, Collection } from "discord.js";
+
+const fs = require('node:fs');
+const path = require('node:path');
+
+const registerClientCommands = (clientCommands: Collection<any, any>) => {
+    const foldersPath = path.join(__dirname, "..", 'commands');
+    const commandFolders = fs.readdirSync(foldersPath);
+
+    for (const folder of commandFolders) {
+        const commandsPath = path.join(foldersPath, folder);
+        const commandFiles = fs.readdirSync(commandsPath).filter((file: any) => file.endsWith('.ts'));
+        for (const file of commandFiles) {
+            const filePath = path.join(commandsPath, file);
+            const command = require(filePath);
+            // Set a new item in the Collection with the key as the command name and the value as the exported module
+            if ('data' in command && 'execute' in command) {
+
+                clientCommands.set(command.data.name, command);
+            } else {
+                console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+            }
+        }
+    }
+}
+
+export default registerClientCommands
