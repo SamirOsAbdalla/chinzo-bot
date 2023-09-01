758f8cec8ec0c317075fd6abc242d586cbe6d997
samir01 <samir.os.abdalla@gmail.com>
2023-09-01 10:52:28 -0700

    Created file to deploy any new commands

---
+++ src/deploy-commands.ts
@@ -0,0 +1,34 @@
+import 'dotenv/config'
+import { testServer } from '../config.json'
+import { Client, Events } from "discord.js"
+import getCommands from "./utils/getCommands"
+import { REST, Routes } from "discord.js"
+
+const commands = getCommands()
+const rest = new REST().setToken(process.env.TOKEN!);
+// and deploy your commands!
+(async () => {
+    try {
+        console.log(`Started refreshing ${commands.length} application (/) commands.`);
+        // The put method is used to fully refresh all commands in the guild with the current set
+
+        // const data = await rest.put(
+        //     Routes.applicationGuildCommands(process.env.CLIENT_ID!, testServer),
+        //     { body: commands },
+        // );
+        // await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID!, testServer), { body: [] })
+        // await rest.put(
+        //     Routes.applicationCommands(process.env.CLIENT_ID!),
+        //     { body: [] },
+        // );
+
+        const data = await rest.put(
+            Routes.applicationCommands(process.env.CLIENT_ID!),
+            { body: commands },
+        );
+        console.log(`Successfully reloaded application (/) commands.`);
+    } catch (error) {
+        // And of course, make sure you catch and log any errors!
+        console.error(`Ready error:${error}`);
+    }
+})();
