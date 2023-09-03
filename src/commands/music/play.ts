7a5bb86d868d254ecca77379dce937228a40b5a5
samir01 <samir.os.abdalla@gmail.com>
2023-09-03 09:36:49 -0700

    Trivial change

--- src/commands/music/play.ts
+++ src/commands/music/play.ts
@@ -18,7 +18,6 @@ module.exports = {
     async execute(interaction: any, player: Player) {
         const channel = interaction.member.voice.channel;
         if (!channel) return interaction.reply({ content: 'You are not connected to a voice channel!', ephemeral: true }); // make sure we have a voice channel
-
         const query = interaction.options.getString('query', true); // we need input/query to play
         // let's defer the interaction as things can take time to process
         await interaction.deferReply();
