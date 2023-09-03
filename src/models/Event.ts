fcbe3d1c320017d737d46dab4e75083265927edd
samir01 <samir.os.abdalla@gmail.com>
2023-09-03 09:37:42 -0700

    Changed model properties to fit with discord's max amount of rows in an embed

--- src/models/Event.ts
+++ src/models/Event.ts
@@ -1,23 +1,23 @@
 import { Schema, models, model } from "mongoose";
 
 
-export interface Event {
+export interface EventType {
     name: string,
     usersAttending: string[],
     description: string,
-    date: string,
-    time: string
+    dateAndTime: string,
     location: string,
+    googleMapsURL: string
 }
 
-const eventSchema = new Schema<Event>({
+const eventSchema = new Schema<EventType>({
     name: { type: String, required: true },
     usersAttending: { type: [String], required: false },
     description: { type: String, required: true },
-    date: { type: String, required: true },
-    time: { type: String, required: true },
+    dateAndTime: { type: String, required: true },
+    googleMapsURL: { type: String, required: true },
     location: { type: String, required: true },
 })
 
-const EventModel = models.Event || model<Event>("Event", eventSchema)
+const EventModel = models.Event || model<EventType>("Event", eventSchema)
 export default EventModel
