b0d5d18c12581cc25ae073a23434f5bfb1fe94ba
samir01 <samir.os.abdalla@gmail.com>
2023-09-05 12:45:23 -0700

    Moved converted time logic into the track time creation to make sorting easier

--- src/commands/tracktime/tracktime.ts
+++ src/commands/tracktime/tracktime.ts
@@ -16,53 +16,71 @@ module.exports = {
         .setDescription("List track times for a certain track")
         .addStringOption(option =>
             option.setName('name')
-                .setDescription('Name of the track')
+                .setDescription("Type 'list' to list all current tracks or a name for a certain track")
                 .setRequired(true)),
+
+
     async execute(interaction: ChatInputCommandInteraction) {
         const trackName = interaction.options.getString("name")
 
-        const response: Track | null = await TrackModel.findOne({ name: { $regex: trackName, $options: 'i' } })
-        if (response) {
-            const trackTimes = response.trackTimes
-            let longestNameLength = -1;
-            if (trackTimes.length > 0) {
+        if (trackName == "list") {
+            const allTracks = await TrackModel.find({})
+            if (allTracks.length == 0) {
+                await interaction.reply({ content: "There are no current tracks!", ephemeral: true })
+                return;
+            }
+            let responseString =
+                `**Here is a list of all of the current tracks**\n\n\n`
+            allTracks.forEach((track, index) => {
+                responseString += `${index + 1}: \`${track.name}\`\n\n`
+            })
+            await interaction.reply({ content: responseString, ephemeral: true })
+        }
+        else {
+            const response: Track | null = await TrackModel.findOne({ name: { $regex: trackName, $options: 'i' } })
+            if (response) {
+                const trackTimes = response.trackTimes
+                let longestNameLength = -1;
+                if (trackTimes.length > 0) {
 
-                const sortedTrackTimes = trackTimes.sort((t1: TrackTime, t2: TrackTime) => {
-
-
-                    if (t1.timeHolder.length > longestNameLength) {
-                        longestNameLength = t1.timeHolder.length
-                    }
-                    return (t1.convertedTime > t2.convertedTime) ? 1 : -1;
-                })
-
-                let responseString =
-                    `Here are all the track times for **${trackName}**\n\n\n`
-
-                sortedTrackTimes.forEach((track: TrackTime) => {
-                    let trackLength = track.time.length - 2
-                    let numSpaces = 7 - trackLength
-                    let spaces: string = " ";
-                    for (let i = 0; i < numSpaces; i++) {
-                        spaces += "  "
-                    }
+                    const sortedTrackTimes = trackTimes.sort((t1: TrackTime, t2: TrackTime) => {
 
 
-                    responseString += `\`${track.time}\`${spaces} **${track.timeHolder}**  *${track.carModel}*\n\n`
-                })
-
-                await interaction.reply({ content: responseString, ephemeral: true })
-            } else {
-                await interaction.reply(
-                    {
-                        content: `There are currently no track times associated with track \`${trackName}\``,
-                        ephemeral: true
+                        if (t1.timeHolder.length > longestNameLength) {
+                            longestNameLength = t1.timeHolder.length
+                        }
+                        return (t1.convertedTime > t2.convertedTime) ? 1 : -1;
                     })
+
+                    let responseString =
+                        `Here are all the track times for **${trackName}**\n\n\n`
+
+                    sortedTrackTimes.forEach((track: TrackTime) => {
+                        let trackLength = track.time.length - 2
+                        let numSpaces = 7 - trackLength
+                        let spaces: string = " ";
+                        for (let i = 0; i < numSpaces; i++) {
+                            spaces += "  "
+                        }
+
+
+                        responseString += `\`${track.time}\`${spaces} **${track.timeHolder}**  *${track.carModel}*\n\n`
+                    })
+
+                    await interaction.reply({ content: responseString, ephemeral: true })
+                } else {
+                    await interaction.reply(
+                        {
+                            content: `There are currently no track times associated with track \`${trackName}\``,
+                            ephemeral: true
+                        })
+                }
+
+                return
             }
 
-            return
+            await interaction.reply({ content: `**ERROR!** Could not find track ${trackName}`, ephemeral: true })
         }
 
-        await interaction.reply({ content: `**ERROR!** Could not find track ${trackName}`, ephemeral: true })
     }
 }
