6d27d95c8a928bc0f6b500fd38ee8bf088453902
samir01 <samir.os.abdalla@gmail.com>
2023-09-10 14:45:12 -0700

    changing dotenv import

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,4 +1,4 @@
-import 'dotenv/config'
+require('dotenv').config()
 import * as fs from 'node:fs';
 import * as path from "node:path"
 import { Client, Collection, GatewayIntentBits } from "discord.js";
