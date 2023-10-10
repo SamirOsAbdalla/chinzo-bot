908fac8a66b603b8c6245801599fd0f5319eecb2
samir01 <samir.os.abdalla@gmail.com>
2023-10-10 07:32:11 -0700

    Removed unecessary command code

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -4,27 +4,14 @@ import getCommands from "../utils/getCommands"
 import { REST, Routes } from "discord.js"
 const rest = new REST().setToken(process.env.TOKEN!);
 const commands = getCommands()
+
 module.exports = {
     name: Events.ClientReady,
     once: true,
     async execute(client: Client) {
-        let testServer = process.env.TEST_SERVER!
-        // console.log(commands)
-        // await rest.put(
-        //     Routes.applicationCommands(process.env.CLIENT_ID!),
-        //     { body: [] },
-        // );
-        // await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID!, testServer), { body: [] })
-        // const data = await rest.put(
-        //     Routes.applicationGuildCommands(process.env.CLIENT_ID!, testServer),
-        //     { body: commands },
-        // );
-        // const data = await rest.put(
-        //     Routes.applicationCommands(process.env.CLIENT_ID!),
-        //     { body: commands },
-        // );
-
         console.log("Greasy Bot is online!")
+
+
     }
 
 }
